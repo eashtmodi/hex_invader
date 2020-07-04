36bb45948be7d3837c045dc4a0bc96d2f7b9dee9
eashtmodi <eashtmodi1006@gmail.com>
2020-07-04 19:36:38 +0530

    hexy

--- Box.js
+++ Box.js
@@ -1,7 +1,21 @@
 class Box extends BaseClass {
   constructor(x, y, width, height){
     super(x,y,width,height);
-    
+    this.visible=255;
   }
+  display(){
+    console.log(this.body.speed);
+    
+    if(this.body.speed<6){
+      super.display();
+    }
+    else{
+      push();
+      World.remove(world,this.body);
+      this.visible=this.visible-10;
+      tint(255,this.visible);
+      pop();
+    }
 
-};
+  }
+}
--- Slingshot.js
+++ Slingshot.js
@@ -4,12 +4,17 @@ class SlingShot{
             bodyA: bodyA,
             pointB: pointB,
             stiffness: 0.04,
-            length: 5
+            length: 50
         }
         this.pointB = pointB;
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
+
+    attach(body){
+        this.sling.bodyA=body;
+    }
+
 
     fly(){
         this.sling.bodyA = null;
